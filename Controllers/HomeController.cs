b12493ba58ce328e29970ba68777da42f49c44a0
Bianca Dollente <biancaadrienne24@gmail.com>
2025-07-24 20:46:42 +0800

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PRELIM_A1_BSIT31A2_Bianca_Dollente.Models;
+
+namespace PRELIM_A1_BSIT31A2_Bianca_Dollente.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace PRELIM_A1_BSIT31A2_Bianca_Dollente.Models
+{
+    public enum Gender
+    {
+        Unknown,
+        Male,
+        Female
+    }
+
+    public class Student
+    {
+        private string firstName;
+        private string lastName;
+
+        public string Title { get; set; }
+        public string Course { get; set; }
+        public string Section { get; set; }
+        public DateTime Birthday { get; set; }
+        public Gender Gender { get; private set; } = Gender.Unknown;
+
+        public string FullName => $"{Title} {firstName} {lastName}";
+        public int Age => DateTime.Now.Year - Birthday.Year -
+            (DateTime.Now.DayOfYear < Birthday.DayOfYear ? 1 : 0);
+
+        public Student()
+        {
+            Title = "Mr./Ms.";
+            firstName = "First";
+            lastName = "Last";
+            Course = "BSIT";
+            Section = "31A1";
+            Birthday = new DateTime(2000, 1, 1);
+        }
+
+        public Student(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public void SetFirstName(string fname) => firstName = fname;
+        public void SetLastName(string lname) => lastName = lname;
+        public void SetGender(Gender gender) => Gender = gender;
+    }
+}
+
